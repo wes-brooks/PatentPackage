be90f9277f8494bf970584e90e85d58c008787ee
Wes B <wbrooks@ucdavis.edu>
2022-10-26 08:46:36 -0700

    switch to using the check-release badge as generated by usethis

--- scratch/manage-build.R
+++ scratch/manage-build.R
@@ -1,6 +1,11 @@
 library("usethis")
 
+# run the test-coverage action
 use_github_action( "test-coverage" )
-use_github_action( "check-release" )
 
+# run release check and add a badge
+use_github_action( "check-release" )
+use_github_actions_badge( "check-release")
+
+# use the MIT liense
 use_mit_license()
